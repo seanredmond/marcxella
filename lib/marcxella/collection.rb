531949b2ed7687594e405b8f4732040505a7279f
Sean Redmond <github-smr@sneakemail.com>
2018-03-28 09:00:05 -0400

    Add documentation

--- lib/marcxella/collection.rb
+++ lib/marcxella/collection.rb
@@ -1,9 +1,13 @@
 module Marcxella
+  # Wrapper for a collection element, which contains 0 or more record
+  #   elements
+  # @since 0.1.0
   class Collection
     def initialize(node)
       @node = node
     end
-    
+
+    # @return [Array<Record>] the records in the container
     def records
       @node.css('record').map{|r| Record.new(r)}
     end
--- lib/marcxella/record.rb
+++ lib/marcxella/record.rb
@@ -1,4 +1,9 @@
 module Marcxella
+
+  # Wrapper for a record element
+  # @since 0.1.0
+  # @attr_reader [Nokogiri::XML::Document] node the wrapped record element node
+  # @attr_reader [String] leader the record leader
   class Record
     attr_reader :node, :leader
     def initialize(node)
@@ -6,6 +11,18 @@ module Marcxella
       @leader = node.css('leader').text
     end
 
+    # Get fields by a tag, or a subfields by a tag and code.
+    # @param tag [String, Integer] Tag of the field to get. There is a potential
+    #   gotcha when using integers  Since MARC field tags are usually shown as
+    #   0-padded, three-digit numbers such as "010", it would be natural to
+    #   pass 010 as the tag, but to Ruby the leading zero means that it is an
+    #   octal number, so 010 means "008".
+    # @param code [String] Subfield code. When not nil, retun value will be an
+    #   Array of SubField objects that are children of the DataField with the
+    #   matching tag and have the matching code. Note that this may return
+    #   subfields of _different_ datafields.
+    # @return [Array<DataField>, Array<ControlField>, Array<SubField>] The
+    #   return type will depend on the tag being requested.
     def field(tag, code = nil)
       if tag.is_a?(Integer)
         tag = "%0.3d" % [tag]
@@ -20,10 +37,15 @@ module Marcxella
       field(tag).map{|f| f.subfield(code)}.flatten
     end
 
+    # The control and data fields of the record
+    # @return [Array<ControlField, DataField>]
     def fields
       @node.css('controlfield, datafield').map{|f| objectify f}
     end
 
+    # Does the record include a field with a specific tag?
+    # @return [boolean] true if there is at least one control or data field
+    #   with the specified tag.
     def include?(tag, code = nil)
       not field(tag, code).empty?
     end
@@ -37,5 +59,7 @@ module Marcxella
 
       raise "Wha?"
     end
+
+    private :objectify
   end
 end
