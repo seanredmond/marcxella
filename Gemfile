7dda80aef233dbafb18b8c73278c4325db01d8f9
Sean Redmond <github-smr@sneakemail.com>
2018-03-27 23:14:20 -0400

    Add yard documentation

--- Gemfile
+++ Gemfile
@@ -7,3 +7,6 @@ gemspec
 
 # Added at 2018-03-27 10:23:20 -0400 by seanredmond:
 gem "nokogiri", "~> 1.8"
+
+# Added at 2018-03-27 23:01:35 -0400 by sean:
+gem "yard", "~> 0.9.12", :group => [:development]
--- lib/marcxella/document.rb
+++ lib/marcxella/document.rb
@@ -1,6 +1,13 @@
 module Marcxella
+
+  # Container for a MARC-XML document
+  # @since 0.1.0
+  # @attr_reader [Nokigiri::XML::Document] document wrapped Nokogiri document
   class Document
     attr_reader :document
+
+    # Constructor for Document object
+    # @param [Nokogiri::XML::Document, File, String] xml source
     def initialize(xml)
       if xml.is_a? Nokogiri::XML::Document
         @document = xml
@@ -9,10 +16,16 @@ module Marcxella
       end
     end
 
+    # Record elements
+    # @return [Array<Record>] array of record objects (possibly empty). If the
+    #   document contains collection elements, returns record objects inside the
+    #   collections
     def records
       @document.css('record').map{|r| Record.new(r)}
     end
 
+    # Collection elements
+    # @return [Array<Collection>] array of collection objects (possible empty).
     def collections
       @document.css('collection').map{|c| Collection.new(c)}
     end
